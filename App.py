daaa6a410de697e3d08f2d97755c5025fc1c5155
Alex <alex@alex.es>
2025-08-03 18:14:59 +0200

    test

--- App.py
+++ App.py
@@ -25,11 +25,5 @@ class App(Gtk.Application):
     def get_application(self):
         return self.win
 
-
-# print(Gtk.get_major_version(), Gtk.get_minor_version(), Gtk.get_micro_version())
-# print(hasattr(Gtk, "PropertySorter"))
-# print(Gtk.MAJOR_VERSION, Gtk.MINOR_VERSION, Gtk.MICRO_VERSION)
-
-
 app = App()
 app.run()
