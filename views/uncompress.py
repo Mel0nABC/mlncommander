1331842bc2800083d6abfb14713836df2cb9babb
Mel0nABC <alex@alex.es>
2025-08-28 19:22:54 +0200

    Reuse lint confirmed

--- views/uncompress.py
+++ views/uncompress.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2025 Mel0nABC
+#
+# SPDX-License-Identifier: MIT
+
 from utilities.i18n import _
 from utilities.compression import CompressionManager
 from controls.actions import Actions
