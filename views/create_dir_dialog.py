d85d82edb218b8b890fcd98f312b3262f0411e54
Mel0nABC <alex@alex.es>
2025-08-31 15:46:33 +0200

    Migrate dir creation from Dialog to Window

--- views/create_dir_dialog.py
+++ views/create_dir_dialog.py
@@ -10,12 +10,13 @@ import asyncio
 gi.require_version("Gtk", "4.0")
 
 
-class Create_dir_dialog(Gtk.Dialog):
+class Create_dir_dialog(Gtk.Window):
     def __init__(
         self,
         parent: Gtk.ApplicationWindow,
         explorer_src: "Explorer",  # noqa: F821
     ):
+        print("CREATE DIR")
         super().__init__(
             title=_("Creando directorio"),
             transient_for=parent,
@@ -28,20 +29,17 @@ class Create_dir_dialog(Gtk.Dialog):
 
         self.set_default_size(horizontal / 5, vertical / 8)
 
-        box = self.get_content_area()
-
         vertical_box_info = Gtk.Box(
             orientation=Gtk.Orientation.VERTICAL, spacing=6
         )
         vertical_box_info.set_margin_top(20)
         vertical_box_info.set_margin_start(20)
+        self.set_child(vertical_box_info)
 
         self.entry_file_name = Gtk.Entry()
         self.entry_file_name.connect("activate", self.get_selected_option)
 
         vertical_box_info.append(self.entry_file_name)
-
-        box.append(vertical_box_info)
 
         self.btn_accept = Gtk.Button(label=_("Aceptar"))
         self.btn_cancel = Gtk.Button(label=_("Cancelar"))
@@ -61,33 +59,27 @@ class Create_dir_dialog(Gtk.Dialog):
         self.vertical_box.append(self.btn_accept)
         self.vertical_box.append(self.btn_cancel)
 
-        box.append(self.vertical_box)
+        vertical_box_info.append(self.vertical_box)
 
         self.response_text = None
         self.future = asyncio.get_event_loop().create_future()
-        self.connect("response", self._on_response)
         self.present()
 
     def exit(self, button: Gtk.Button) -> None:
         """
         Close dialog on press cancel button
         """
-        self.close()
+        self.destroy()
 
     def get_selected_option(self, botton: Gtk.Button) -> None:
         """
         Set on variable entry value
         """
         self.response_text = self.entry_file_name.get_text()
-        self.close()
-
-    def _on_response(self, dialog: Gtk.Dialog, response_id: str) -> None:
-        """
-        Set response on close dialog
-        """
         if not self.future.done():
             self.future.set_result(self.response_text)
-        self.close()
+
+        self.destroy()
 
     async def wait_response_async(self) -> None:
         """
