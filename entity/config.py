4b95e00f3b9a960c1d99c39c947aae5b9b5f389c
Mel0nABC <alex@alex.es>
2025-09-07 10:59:57 +0200

    Refactored

--- entity/config.py
+++ entity/config.py
@@ -14,63 +14,40 @@ class ConfigEntity(GObject.Object):
 
     def __init__(
         self,
-        SWITCH_COPY_STATUS: bool = None,
-        SWITCH_MOVE_STATUS: bool = None,
-        SWITCH_DUPLICATE_STATUS: bool = None,
-        SWITCH_COMPRESS_STATUS: bool = None,
-        SWITCH_UNCOMPRESS_STATUS: bool = None,
-        EXP_1_PATH: str = None,
-        EXP_2_PATH: str = None,
-        FAV_PATH_LIST_1: list[str] = None,
-        FAV_PATH_LIST_2: list[str] = None,
-        SHOW_DIR_LAST: bool = None,
-        SWITCH_IMG_STATUS: bool = None,
-        SWITCH_WATCHDOG_STATUS: bool = None,
-        COLOR_BACKGROUND_APP: str = None,
-        COLOR_ENTRY: str = None,
-        COLOR_EXPLORER_LEFT: str = None,
-        COLOR_EXPLORER_RIGHT: str = None,
-        COLOR_BUTTON: str = None,
-        COLOR_FAV_BUTTON: str = None,
-        COLOR_BACKGROUND_SEARCH: str = None,
-        COLOR_SEARCH_TEXT: str = None,
-        FONT_STYLE: str = None,
-        FONT_STYLE_COLOR: str = None,
-        LANGUAGE: str = None,
     ):
         super().__init__()
 
         # GENERAL
 
-        self.SWITCH_COPY_STATUS = SWITCH_COPY_STATUS
-        self.SWITCH_MOVE_STATUS = SWITCH_MOVE_STATUS
-        self.SWITCH_DUPLICATE_STATUS = SWITCH_DUPLICATE_STATUS
-        self.SWITCH_COMPRESS_STATUS = SWITCH_COMPRESS_STATUS
-        self.SWITCH_UNCOMPRESS_STATUS = SWITCH_UNCOMPRESS_STATUS
-        self.LANGUAGE = LANGUAGE
+        self.SWITCH_COPY_STATUS = None
+        self.SWITCH_MOVE_STATUS = None
+        self.SWITCH_DUPLICATE_STATUS = None
+        self.SWITCH_COMPRESS_STATUS = None
+        self.SWITCH_UNCOMPRESS_STATUS = None
+        self.LANGUAGE = None
 
         # DIRECTORYS
 
-        self.FAV_PATH_LIST_1 = FAV_PATH_LIST_1
-        self.FAV_PATH_LIST_2 = FAV_PATH_LIST_2
-        self.EXP_1_PATH = EXP_1_PATH
-        self.EXP_2_PATH = EXP_2_PATH
-        self.SHOW_DIR_LAST = SHOW_DIR_LAST
-        self.SWITCH_IMG_STATUS = SWITCH_IMG_STATUS
-        self.SWITCH_WATCHDOG_STATUS = SWITCH_WATCHDOG_STATUS
+        self.FAV_PATH_LIST_1 = None
+        self.FAV_PATH_LIST_2 = None
+        self.EXP_1_PATH = None
+        self.EXP_2_PATH = None
+        self.SHOW_DIR_LAST = None
+        self.SWITCH_IMG_STATUS = None
+        self.SWITCH_WATCHDOG_STATUS = None
 
         # APPEARANCE
 
-        self.COLOR_BACKGROUND_APP = COLOR_BACKGROUND_APP
-        self.COLOR_ENTRY = COLOR_ENTRY
-        self.COLOR_EXPLORER_LEFT = COLOR_EXPLORER_LEFT
-        self.COLOR_EXPLORER_RIGHT = COLOR_EXPLORER_RIGHT
-        self.COLOR_BUTTON = COLOR_BUTTON
-        self.COLOR_FAV_BUTTON = COLOR_FAV_BUTTON
-        self.COLOR_BACKGROUND_SEARCH = COLOR_BACKGROUND_SEARCH
-        self.COLOR_SEARCH_TEXT = COLOR_SEARCH_TEXT
-        self.FONT_STYLE = FONT_STYLE
-        self.FONT_STYLE_COLOR = FONT_STYLE_COLOR
+        self.COLOR_BACKGROUND_APP = None
+        self.COLOR_ENTRY = None
+        self.COLOR_EXPLORER_LEFT = None
+        self.COLOR_EXPLORER_RIGHT = None
+        self.COLOR_BUTTON = None
+        self.COLOR_FAV_BUTTON = None
+        self.COLOR_BACKGROUND_SEARCH = None
+        self.COLOR_SEARCH_TEXT = None
+        self.FONT_STYLE = None
+        self.FONT_STYLE_COLOR = None
 
     def to_dict(self) -> dict:
         return {
