2e07b42d12bba8bcc94cf78118d6df46def61447
Mel0nABC <alex@alex.es>
2025-09-10 16:58:28 +0200

    Access to information about system users and groups

---
+++ utilities/sistem_info.py
@@ -0,0 +1,15 @@
+import pwd
+import grp
+
+
+class SistemInformation:
+
+    def get_sistem_users():
+        user_list = [user.pw_name for user in pwd.getpwall()]
+        user_list.sort()
+        return user_list
+
+    def get_sistem_groups():
+        group_list = [group.gr_name for group in grp.getgrall()]
+        group_list.sort()
+        return group_list
